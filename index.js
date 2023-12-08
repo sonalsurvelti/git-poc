64e880f59be05e9949ffcf9e73f8ca496bff8a21
sonalsurvelti <Sonal.Surve@lntinfotech.com>
2023-12-08 13:20:37 +0530

    prod world changes

--- index.js
+++ index.js
@@ -5,9 +5,10 @@ const port = 3000
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
-app.get('/test', (req, res) => {
-    res.send('Hello test World!')
+app.get('/prod', (req, res) => {
+    res.send('Hello prod World!')
   })
+
 
 //add changes that needs to be pushed to test branch
 
