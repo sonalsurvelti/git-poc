60fdd7f1aa47e84ca33288d46aeb8c065aadf9ca
sonalsurvelti <Sonal.Surve@lntinfotech.com>
2023-12-18 12:32:10 +0530

    changes

--- index.js
+++ index.js
@@ -5,15 +5,14 @@ const port = 3000
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+
+app.get('/test', (req, res) => {
+    res.send('Hello prod World!')
+})
+
 app.get('/prod', (req, res) => {
     res.send('Hello prod World!')
-  })
-
-
-//add changes that needs to be pushed to test branch
-  app.get('/test123', (req, res) => {
-    res.send('Hello test123 World!')
-  })
+})
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
